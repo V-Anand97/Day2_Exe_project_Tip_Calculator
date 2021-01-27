235aad84318f6f540ab148403601936cd82901d7
V-Anand97 <74920428+V-Anand97@users.noreply.github.com>
2021-01-27 15:01:54 +0530

    Initial commit

---
+++ Day 2 Exe 2.1.py
@@ -0,0 +1,7 @@
+two_digit = input('Enter a two digit number')
+print(type(two_digit))
+first_digit = two_digit[0]
+second_digit = two_digit[1]
+sum = int(first_digit) +int(second_digit)
+print("the sum of the two digit number", two_digit ,'is', sum)
+# print(sum)
---
+++ Day 2 Exe 2.2 BMI Calculator.py
@@ -0,0 +1,5 @@
+print("BMI Calculator")
+weight =  float(input('Enter your weight in KG: \n'))
+height = float(input("Enter your height in meters: \n"))
+BMI = int(weight / (height ** 2))
+print('your BMI is',BMI)
