383c1e57113a6279f309c6e976115c7614f4aba2
V-Anand97 <74920428+V-Anand97@users.noreply.github.com>
2021-01-27 15:40:36 +0530

    Final code of this program

---
+++ Day2_Exe_2.3_Life_in_weeks.py
@@ -0,0 +1,7 @@
+print("Life In Weeks")
+age= int(input('how old are you?\n'))
+remainder = 90 - age
+months = int(remainder*12)
+weeks = int(remainder*52)
+days = int(remainder*365)
+print(f" You have {days} days, {weeks} weeks and {months } months left")
